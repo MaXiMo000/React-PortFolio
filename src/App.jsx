3270855c2fe1bad5249d96004dd056b2cd89c2d7
MaXiMo000 <109451965+MaXiMo000@users.noreply.github.com>
2024-08-18 17:57:35 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './app.scss';
 import Contact from './components/contact/Contact';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
-import Parallax from './components/parallax/parallax';
+import Parallax from './components/parallax/Parallax';
 import Portfolio from './components/portfolio/Portfolio';
 import Services from './components/services/Services';
 
